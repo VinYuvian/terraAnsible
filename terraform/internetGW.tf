ba96e727d747f1d2dbb768c394ca9d75a89f6715
Ubuntu <ubuntu@ip-172-31-86-215.ec2.internal>
2019-11-27 07:41:09 +0000

    removed

--- terraform/internetGW.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_internet_gateway" "mainGW"{
-	vpc_id="${aws_vpc.mainVPC.id}"
-	tags={
-		Name="mainGateway"
-	}
-}
--- terraform/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "aws"{
-	version = "~> 2.39"
-	access_key="${var.AWS_ACCESS_KEY}"
-	secret_key="${var.AWS_SECRET_KEY}"
-	region="${var.region}"
-}
