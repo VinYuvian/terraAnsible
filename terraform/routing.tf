45a15dfbb985fcc5efc27f92cc957334defffc90
Ubuntu <ubuntu@ip-172-31-86-215.ec2.internal>
2019-11-27 10:44:39 +0000

    edited nat gateway

--- terraform/routing.tf
+++ terraform/routing.tf
@@ -21,7 +21,7 @@ resource "aws_route" "publicRoute"{
 resource "aws_route" "privateRoute"{
 	route_table_id="${aws_route_table.privateRoute.id}"
 	destination_cidr_block="0.0.0.0/0"
-	gateway_id="${aws_nat_gateway.natGW.id}"
+	nat_gateway_id="${aws_nat_gateway.natGW.id}"
 }
 
 resource "aws_route_table_association" "publicAssociation"{
