13b6df5a1a6e57d695391e40ee334abd08428d1f
Ubuntu <ubuntu@ip-172-31-86-215.ec2.internal>
2019-11-27 12:41:07 +0000

    added instance.tf

---
+++ terraform/instance.tf
@@ -0,0 +1,37 @@
+resource "aws_key_pair" "webKey"{
+	key_name="webKey"
+	public_key="${file("${var.PUBLIC_WEB_KEY}")}"
+}
+
+resource "aws_security_group" "webSG"{
+	vpc_id="${aws_vpc.mainVPC.id}"
+	#subnet_id="${aws_subnet.publicSubnets["10.0.1.0/24"]}"
+	ingress{
+		from_port=22
+		to_port=22
+		protocol="tcp"
+		cidr_blocks=["0.0.0.0/0"]
+
+	}
+	egress{
+		from_port=0
+		to_port=0
+		protocol="-1"
+		cidr_blocks=["0.0.0.0/0"]
+	}
+	tags={
+		Name="webSG"
+	}
+}
+
+resource "aws_instance" "webApp"{
+	subnet_id="${aws_subnet.publicSubnets["10.0.1.0/24"].id}"
+	security_groups=["${aws_security_group.webSG.id}"]
+	instance_type="t2.micro"
+	user_data="${file("user.sh")}"
+	key_name="${aws_key_pair.webKey.key_name}"
+	ami="ami-04b9e92b5572fa0d1"
+	tags={
+		Name="webApp"
+	}
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,6 +6,7 @@ variable "keyname"{
 }
 variable "PUBLIC_KEY_PATH"{}
 
+variable "PUBLIC_WEB_KEY"{}
 variable "subnetValues"{
 	type="list"
 	default=["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24","10.0.4.0/24","10.0.5.0/24","10.0.6.0/24"]
